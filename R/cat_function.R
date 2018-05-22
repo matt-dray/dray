5e9d49b92846186022a0359b7d9391f7d9f7b4ad
Matt Dray <mwdray@gmail.com>
2018-05-22 19:26:46 +0100

    add function following hilary parker

---
+++ R/cat_function.R
@@ -0,0 +1,20 @@
+#' A Cat Function
+#'
+#' This function allows you to express your love of cats.
+#' @param love Do you love cats? Defaults to TRUE.
+#' @keywords cats
+#' @export
+#' @examples
+#' cat_function()
+
+cat_function <- function(love=TRUE){
+  if(love==TRUE){
+    print("I love cats!")
+  }
+  else {
+    print("I am not a cool person.")
+  }
+}
+
+# thanks hilary
+# https://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/
