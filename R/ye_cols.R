fd434bc8b5dfa80fae029a404be15d4e4e4c2f4d
Matt Dray <mwdray@gmail.com>
2018-06-07 10:58:48 +0100

    fix commas

--- R/ye_cols.R
+++ R/ye_cols.R
@@ -25,12 +25,12 @@
 #'
 
 ye_cols <- c(
-  mountain_blue = "#233956" # Catalina Blue (Blue)
-  grass_blue = "#0e1e27"  # Black Pearl (Blue)
-  cloud_blue1 = "#7a8aa2"  # Ship Cove (Blue)
-  cloud_white = "#dfd7c9"  # Albescent White (White)
-  cloud_grey = "#b5b2b0"  # Bombay (Grey)
-  cloud_blue = "#9da3ae"  # Spun Pearl (Blue)
+  mountain_blue = "#233956", # Catalina Blue (Blue)
+  grass_blue = "#0e1e27",  # Black Pearl (Blue)
+  cloud_blue1 = "#7a8aa2",  # Ship Cove (Blue)
+  cloud_white = "#dfd7c9",  # Albescent White (White)
+  cloud_grey = "#b5b2b0",  # Bombay (Grey)
+  cloud_blue = "#9da3ae",  # Spun Pearl (Blue)
   text_green = "#31ef56"  # Malachite (Green)
 )
 
