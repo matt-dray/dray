9cf905ded29215b302298ecdefc05dd400fd6c63
Matt Dray <mwdray@gmail.com>
2018-06-18 22:42:30 +0100

    add example of data file to package

---
+++ R/pkmn_go_data.R
@@ -0,0 +1,20 @@
+#' Pokemon Go Captures.
+#'
+#' Details of nearly 700 Pokemon captured in Autumn 2016 on the Pokemon Go
+#' video game app on Apple iOS.
+#'
+#' @docType data
+#'
+#' @usage data(pkmn_go)
+#'
+#' @keywords datasets
+#'
+#' @references Pokemon Go, developed and published by Niantic, with characters
+#' developed by The Pokemon Company.
+#' (\href{https://www.nianticlabs.com/}{Niantic})
+#'
+#' @source \href{https://www.pokemongo.com/}{Pokemon Go}
+#'
+#' @examples
+#' data(pkmn_go)
+"pkmn_go"
