ae629f45ded01d69f63bde3d7e120f84997ae6b1
Matt Dray <mwdray@gmail.com>
2018-06-30 15:26:53 +0100

    add phd data

---
+++ R/phd_data.R
@@ -0,0 +1,21 @@
+#' PhD Thesis By Matt Dray.
+#'
+#' Text of the PhD thesis 'Effects of multiple environmental stressors on litter
+#' chemical composition and decomposition' (Dray, 2014). Character vector where
+#' each element is a line of text.
+#'
+#' @docType data
+#'
+#' @usage data(phd)
+#'
+#' @keywords datasets
+#'
+#' @references Dray, Matthew William (2014). Effects of multiple environmental
+#' stressors on litter chemical composition and decomposition. PhD thesis,
+#' Cardiff University.
+#'
+#' @source \href{http://orca.cf.ac.uk/id/eprint/68365/}{Orca online research portal}
+#'
+#' @examples
+#' data(phd)
+"phd"
