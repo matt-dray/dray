100cb024ca053866ae934612ce3985c2f0783001
Matt Dray <mwdray@gmail.com>
2018-07-24 23:21:09 +0100

    add example - not run

--- R/make_gif.R
+++ R/make_gif.R
@@ -1,14 +1,26 @@
 #' Make a gif from a folder of PNG image files.
 #'
-#' Point to a folder of PNG files and stitch them into a gif. Convenience function for the gifski function.
-#' @param png_folder Folder where you've stored your PNG image files.
-#' @param gif_path File path for your new gif, must end in '.gif' extension.
+#' Point to a folder of PNG files and engifify them (make them into a gif (I made this word up)). A convenience function for the gifski function.
+#' @param png_folder Folder where you've stored your PNG image files (.png or .PNG).
+#' @param gif_path File path for your new gif including .gif extension.
 #' @param ... Pass further arguments to gifski (see ?gifski for details).
 #' @keywords gif, gifski
 #' @import gifski
 #' @export
 #' @examples
-#' # TODO
+#' \dontrun{
+#' # filepath of folder containing PNGs
+#' gif_in <- "folder/subfolder"
+#'
+#' # filepath for the output gif (including extension)
+#' gif_out <- "gifs/amazing_gif.gif"
+#'
+#'# engifify
+#' make_gif(
+#'   png_folder = gif_in,
+#'   gif_path = gif_out
+#' )
+#' }
 
 
 make_gif <- function(
@@ -23,6 +35,7 @@ make_gif <- function(
 
   png_files <- list.files(
     path = png_folder,
+    pattern = ".png|.PNG",
     full.names = TRUE
   )
 
